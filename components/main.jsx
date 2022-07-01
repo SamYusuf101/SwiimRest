073b9ad157b7eb373f09ae01681c94e85a765b26
zilla <samyusuf101@gmail.com>
2022-07-01 08:19:10 -0700

    created about component

---
+++ components/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div className='w-full h-screen text-center'>
+      <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+        <div>
+        <p className='uppercase text-sm tracking-widest text-gray-600'>
+        Let's build something together
+        </p>
+         <h1 className='py-8 text-red-700'>
+         Hi, I'm <span className='text-[#5651e5]'>Sam</span>
+         </h1> 
+         
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Main;
