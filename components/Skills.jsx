8719e774f7a43bf1542f9198101d65274fa5a501
zilla <samyusuf101@gmail.com>
2022-07-01 09:10:52 -0700

    updated Skills

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+
 
 const Skills = () => {
-  return (
-    <div className='w-full lg:h-screen p-2'>
-      <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
-         <p className='text-xl tracking-widest'>Skills</p>
-         <h2>What i can do</h2>
-         <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'></div>
-      </div>
-    </div>
-  )
+    return (
+        <div className='w-full lg:h-screen p-2'>
+            <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
+                <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Skills</p>
+                <h2 className='py-4'>What i can do</h2>
+                <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
+                    <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                    <div className='m-auto'>
+                       <Image src="/../public/assets/html.png" alt="/" width='64px' height='64px' />
+                       
+                    </div> 
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Skills
