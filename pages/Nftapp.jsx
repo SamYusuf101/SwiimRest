97a4e1e47b4eb8c62cf20426ce6ef85db5eb3fc4
zilla <samyusuf101@gmail.com>
2022-07-14 05:14:06 -0700

    update

--- pages/Nftapp.jsx
+++ pages/Nftapp.jsx
@@ -13,7 +13,7 @@ const Nftapp = () => {
        <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10' />
        <Image className='absolute z-1' layout='fill' objectFit='cover' src={nftImg} alt='/' />
        <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
-         <h2 className='py-2'>Nftttttt App</h2>
+         <h2 className='py-2'>Nfttttthhhhht App</h2>
          <h3>React Native</h3>
        </div>
      </div>
