88de8d80113ee38479ead2a5df8193d59e049bb4
zilla <samyusuf101@gmail.com>
2022-07-15 09:11:31 -0700

    updated

--- pages/Newsapp.jsx
+++ pages/Newsapp.jsx
@@ -14,7 +14,7 @@ const Newsapp = () => {
        <Image className='absolute z-1' layout='fill' objectFit='cover' src={newsImg} alt='/' />
        <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
          <h2 className='py-2'>News App</h2>
-         <h3>React Native</h3>
+         <h3>React Native.</h3>
        </div>
      </div>
       <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8'>
